00c17bf1a3b066e0ad28a0cc49a207e374205729
cvalencia86 <cristian.valencia@e360.com>
2025-09-08 14:02:37 -0400

    added workflow terraform fixed provider

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.12.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
